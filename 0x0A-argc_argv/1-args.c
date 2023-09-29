a3e2f56524aa66a51e1c3f373d79bf93113f537d
Chepkemoibrendak <breezykerby3@gmail.com>
2023-09-29 12:49:14 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,8 +7,9 @@
  * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
-{
-	(void) argv; /*Ignore argv*/
-	printf("%d\n", argc - 1);
-	return (0);
-}
+{      
+        (void) argv; /*Ignore argv*/
+        printf("%d\n", argc - 1);
+        return (0);
+}       
+
