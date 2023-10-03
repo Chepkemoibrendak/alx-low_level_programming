83a944b7766a1d3162c71fbbee4f69a2c6f1a8af
Chepkemoibrendak <breezykerby3@gmail.com>
2023-10-03 08:42:59 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,7 +20,7 @@ int **alloc_grid(int width, int height)
 		free(a);
 		return (NULL);
 	}
-	for (i = 0; i < height; i+=)
+	for (i = 0; i < height; i++)
 	{
 		a[i] = malloc(sizeof(int) * width);
 		if (a[i] == NULL)
